486d4ef3b6ecea23984512de14ff7a0d7afce6df
BavlaR <vdmmailbox1@gmail.com>
2021-01-22 16:30:55 +0200

    task1

--- lesson4/task1/index.js
+++ lesson4/task1/index.js
@@ -1,5 +1,5 @@
 import calcResult, { sum, mult } from './calculator/index.js';
-import { fetchUser } from './profile/gateway.js';
+import fetchUser from './profile/gateway.js';
 import { printProfile } from './profile/index.js';
 import './polyfills/array-flat.js';
 import './polyfills/array-flatMap.js';
